d0ceb0ab00864ff6b60f8a126f40e948b2c67641
youfang <981545521@qq.com>
2017-12-08 16:45:18 +0800

    通用mapper生成器

---
+++ src/main/java/cn/mbg/MybatisGenerator.java
@@ -0,0 +1,28 @@
+package cn.mbg;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author youfang
+ * @date 2017-12-08 15:52
+ **/
+public class MybatisGenerator {
+
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(MybatisGenerator.class.getResourceAsStream("/mbg/generatorConfiguration.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
